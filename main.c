9ccc7c49005e40fac4befc6b0df827b50b06716a
Łukasz Wielgosz <lukasz.wielgosz9@gmail.com>
2021-12-09 08:24:22 +0100

    initial commit with usb cdc driver

---
+++ main.c
@@ -0,0 +1,103 @@
+/*
+    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+
+#include "ch.h"
+#include "hal.h"
+#include "usbcfg.h"
+//#include "rt_test_root.h"
+//#include "oslib_test_root.h"
+
+/*
+ * This is a periodic thread that does absolutely nothing except flashing
+ * a LED.
+ */
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg) {
+
+  (void)arg;
+  chRegSetThreadName("blinker");
+  while (true) {
+    palSetLine(LINE_LED1);
+    chThdSleepMilliseconds(50);
+    palSetLine(LINE_LED2);
+    chThdSleepMilliseconds(50);
+    palSetLine(LINE_LED3);
+    chThdSleepMilliseconds(200);
+    palClearLine(LINE_LED1);
+    chThdSleepMilliseconds(50);
+    palClearLine(LINE_LED2);
+    chThdSleepMilliseconds(50);
+    palClearLine(LINE_LED3);
+    chThdSleepMilliseconds(200);
+  }
+}
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /*
+   * Activates the serial driver 1 using the driver default configuration.
+   */
+  sdStart(&SD3, NULL);
+
+  /*
+   * Initializes a serial-over-USB CDC driver.
+   */
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg);
+
+  /*
+   * Activates the USB driver and then the USB bus pull-up on D+.
+   * Note, a delay is inserted in order to not have to disconnect the cable
+   * after a reset.
+   */
+  usbDisconnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg.usbp, &usbcfg);
+  usbConnectBus(serusbcfg.usbp);
+
+  /*
+   * Creates the example thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1, Thread1, NULL);
+
+  /*
+   * Normal main() thread activity, in this demo it does nothing except
+   * sleeping in a loop and check the button state.
+   */
+  while (1) {
+    if (palReadLine(LINE_BUTTON)) {
+//      test_execute((BaseSequentialStream *)&SD3, &rt_test_suite);
+//      test_execute((BaseSequentialStream *)&SD3, &oslib_test_suite);
+
+      chprintf(&SDU1, "Hello world from USB \r\n");
+    }
+    chThdSleepMilliseconds(500);
+  }
+}
